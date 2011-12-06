0ff631b48f79b7c4995293137121599baad8601e
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-05 22:42:33 -0500

    Full skeleton.

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+require 'librarian/chef/integration/knife'
+current_dir = File.dirname(__FILE__)
+
+cookbook_path = Librarian::Chef.install_path
+
+cookbook_copyright       'Myplanet Digital'
+cookbook_email           'devops@myplanetdigital.com'
+cookbook_license         'apachev2'
---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant::Config.run do |config|
+  config.vm.box = "lucid64"
+
+  config.vm.provision :chef_solo do |chef|
+    chef.cookbooks_path = "cookbooks"
+    chef.add_recipe "freight"
+ 
+    # chef.json = {
+    #   :mysql_password => "foo"
+    # }
+  end
+end
